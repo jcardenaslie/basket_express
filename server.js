d4a4204c3c215ea23c8df0fd193214ed9d5dc590
Joaquin Cardenas <joaquin.cardenas@almacenguru.com>
2021-02-21 14:23:28 -0300

    Add dotenv package to manage environment variables

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const PORT = 4000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
