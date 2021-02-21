451c5d10942677d5128be404cdcd986eccf4568f
Joaquin Cardenas <joaquin.cardenas@almacenguru.com>
2021-02-21 14:20:30 -0300

    Init server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
